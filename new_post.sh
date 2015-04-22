939a7b6e62f72de74c76963860e298b5eff9e2b2
redllamas <sam@jetsets.jp>
2015-04-22 13:21:52 +0900

    added new_post.sh

---
+++ new_post.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+date_pattern=$(date "+%Y-%m-%d")-
+
+read -r -p "Post name > "
+title=${REPLY}
+# to ascii, to lowercase, keep only alphanum and ._- space and turn spaces into dashes
+clean_title=$(echo $title \
+    | iconv -f utf8 -t ascii//translit \
+    | tr '[:upper:]' '[:lower:]' \
+    | tr -dc 'a-z0-9. _-' \
+    | tr ' ' '-')
+
+filename=$date_pattern$clean_title.md
+author=$(git config --get user.name)
+
+cat > "posts/"$filename <<EOF
+---
+title: $title
+author: $author
+tags:
+---
+
+EOF
+
+vim "posts/"$filename
